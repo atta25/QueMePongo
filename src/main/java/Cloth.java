c2451179a9d5a4cb3eb89a0c66ad9bd51880ba13
Juan Carlos Camacho ruiz <juan.camacho@despegar.com>
2020-05-26 21:37:26 -0300

    utilizo enums con campos en Cloth

--- src/main/java/Cloth.java
+++ src/main/java/Cloth.java
@@ -3,57 +3,20 @@ import java.util.Collections;
 import java.util.List;
 
 public enum Cloth {
-    COTTON {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Collections.singletonList(Type.SHIRT);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    },
+    COTTON(Collections.singletonList(Type.SHIRT)),
+    LEATHER(Arrays.asList(Type.JACKET, Type.SHOE, Type.TROUSER)),
+    SILK(Arrays.asList(Type.SHIRT, Type.TSHIRT)),
+    PIQUE(Collections.singletonList(Type.CHOMBA)),
+    CANVAS(Collections.singletonList(Type.SNEAKER)),
+    ACETATE(Collections.singletonList(Type.TROUSER));
 
-    LEATHER {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Arrays.asList(Type.JACKET, Type.SHOE, Type.TROUSER);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    },
+    private final List<Type> supportedTypes;
 
-    SILK {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Arrays.asList(Type.SHIRT, Type.TSHIRT);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    },
+    Cloth(List<Type> supportedTypes) {
+        this.supportedTypes = supportedTypes;
+    }
 
-    PIQUE {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Collections.singletonList(Type.CHOMBA);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    },
-
-    CANVAS {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Collections.singletonList(Type.SNEAKER);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    },
-
-    ACETATE {
-        @Override
-        public boolean isConsistentFor(Type type) {
-            List<Type> compatibleGarmentTypes = Collections.singletonList(Type.TROUSER);
-            return this.containsType(compatibleGarmentTypes, type);
-        }
-    };
-
-    public abstract boolean isConsistentFor(Type type);
-
-    protected Boolean containsType(List<Type> types, Type type) {
-        return types.contains(type);
+    public boolean isConsistentFor(Type type) {
+        return this.supportedTypes.contains(type);
     }
 }
