319755adf288e546cf7468074ccb21e29f83808f
Juan Carlos Camacho ruiz <juan.camacho@despegar.com>
2020-05-26 21:15:15 -0300

    cambio predicates por métodos

--- src/main/java/AttireGenerator.java
+++ src/main/java/AttireGenerator.java
@@ -2,37 +2,44 @@ import com.google.common.collect.Sets;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class AttireGenerator {
     public Set<Attire> getSuggestions(Set<Garment> garments, Double currentTemperature) {
-        Predicate<Set<Garment>> doesNotRepeatGarmentTypes = garmentsSet -> {
-            long quantityOfDifferentGarmentTypes = garmentsSet.stream().map(Garment::getType).distinct().count();
-            return quantityOfDifferentGarmentTypes == garmentsSet.size();
-        };
+        return Sets.powerSet(garments).stream().filter(garmentsSet -> isValidAttire(garmentsSet, currentTemperature)).map(Attire::new).collect(Collectors.toSet());
+    }
 
-        Predicate<Set<Garment>> haveNecessaryClothes = garmentsSet -> {
-            Set<Category> mandatoryCategories = new HashSet<>(Arrays.asList(Category.HIGHER, Category.LOWER, Category.FOOTWEAR));
+    private Boolean isValidAttire(Set<Garment> garments, Double currentTemperature) {
+        return doesNotRepeatGarmentTypes(garments) &&
+                haveNecessaryClothes(garments) &&
+                doesNotRepeatGarmentCategories(garments) &&
+                isSuitable(garments, currentTemperature);
+    }
 
-            return garmentsSet.stream()
-                              .map(garment -> garment.getType().getCategory())
-                              .collect(Collectors.toList())
-                              .containsAll(mandatoryCategories);
-        };
+    private Boolean doesNotRepeatGarmentTypes(Set<Garment> garments) {
+        long quantityOfDifferentGarmentTypes = garments.stream().map(Garment::getType).distinct().count();
+        return quantityOfDifferentGarmentTypes == garments.size();
+    }
 
-        Predicate<Set<Garment>> doesNotRepeatGarmentCategories = garmentsSet -> garmentsSet.stream()
-                                                                                           .collect(groupingBy(garment -> garment.getType().getCategory(), counting()))
-                                                                                           .entrySet()
-                                                                                           .stream()
-                                                                                           .allMatch(map -> map.getValue() == 1);
+    private Boolean haveNecessaryClothes(Set<Garment> garments) {
+        Set<Category> mandatoryCategories = new HashSet<>(Arrays.asList(Category.HIGHER, Category.LOWER, Category.FOOTWEAR));
+        return garments.stream()
+                .map(garment -> garment.getType().getCategory())
+                .collect(Collectors.toList())
+                .containsAll(mandatoryCategories);
+    }
 
-        Predicate<Set<Garment>> isSuitable = garmentsSet -> garmentsSet.stream().map(Garment::getType).allMatch(type -> type.isSuitable(currentTemperature));
+    private Boolean doesNotRepeatGarmentCategories(Set<Garment> garments) {
+        return garments.stream()
+                .collect(groupingBy(garment -> garment.getType().getCategory(), counting()))
+                .entrySet()
+                .stream()
+                .allMatch(map -> map.getValue() == 1);
+    }
 
-        Predicate<Set<Garment>> isValidAttire = doesNotRepeatGarmentTypes.and(haveNecessaryClothes).and(doesNotRepeatGarmentCategories).and(isSuitable);
-
-        return Sets.powerSet(garments).stream().filter(isValidAttire).map(Attire::new).collect(Collectors.toSet());
+    private Boolean isSuitable(Set<Garment> garments, Double currentTemperature) {
+        return garments.stream().map(Garment::getType).allMatch(type -> type.isSuitable(currentTemperature));
     }
 }
